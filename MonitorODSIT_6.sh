34c72a72745709f2edcf4f8027ea56fce05d3bca
Billwolfe <hewolfe325@gmail.com>
2017-07-26 12:57:57 -0700

    Updates Services added

--- MonitorODSIT_6.sh
+++ MonitorODSIT_6.sh
@@ -8,11 +8,16 @@
 #####################################################################
 # Set Variables
 #
-ARSBIN="/apps/IBM/ondemand/V9.5/bin"
-TSMBIN="/apps/IBM/tivoli/tsm/"
-LOCK="/app/IBM/locks/CMODStart.lock"
-LOGFILE="/app/IBM/logs/failover.log"
-USER="CMOSCHEM"
+readonly FTS="SDC01TCMOAPP06"
+readonly ODF="SDC01TCM0APP07"
+readonly ARSBIN="/apps/IBM/ondemand/V9.5/bin"
+readonly TSMBIN="/apps/IBM/tivoli/tsm/"
+readonly ODFBIN="/apps/IBM/bin"
+mkdir -p "/apps/IBM/locks"
+readonly LOCK="/app/IBM/locks/CMODStart.lock"
+mkdir -p "/apps/IBM/logs"
+readonly LOGFILE="/app/IBM/logs/failover.log"
+readonly USER="CMOSCHEM"
 servers=(SDC01TCMOAPP02 SDC01TCMOAPP03)
 #####################################################################
 
@@ -21,6 +26,12 @@ servers=(SDC01TCMOAPP02 SDC01TCMOAPP03)
 ### DEFINE FUNCTIONS ################################################
 # Define function to check if arssockd is running, set tested machine
 # to active if RC 0, set to standby if RC > 0
+
+#FOR SIT ONLY
+arsdocQuery() {
+  arsdoc query -h $LOCAL -u Admin -p /apps/IBM/ondemand/V9.5/config/ars.stash -f "System Log" -H > dev/null
+}
+
 checkCMOD() {
   arssockd -h $1 -P 2>1
   if [[ $? -eq 0 ]]; then
@@ -40,11 +51,58 @@ checkLock() {
 	return 0
 fi;
 }
-# Function to start ODF
-Start ODF function here
-# Function to start FTS
-Start FTS function 
 
+#Function to check TSM, start on Local if not running
+startTSM() {
+if [[ $(ps -ef | grep -c [d]smserv ) == 0 ]] 2>1; then
+  "$TSMBIN"/dsmserv -quiet &
+  sleep 1
+    if [[ $? = 0 ]]; then
+      echo "TSM Server started on "$1" at $(date)" >> $LOGFILE
+      return 0
+    else
+      echo "TSM Server failed to start at $(date)" >> $LOGFILE
+      # exit 1
+    fi
+  else
+    echo "TSM Server running on "$LOCAL" at $(date)" >> $LOGFILE
+    return 0
+fi
+}
+
+# Function to start ODF
+startODF() {
+  SSH -qn "$USER"@${ODF} cd /
+  if [[ $(ps -ef | grep -c [a]rsodf) == 0 ]]; then
+    nohup "$ARSBIN"/arsodf -h "$1" -S &
+    sleep 1
+      if [[ $? = 0 ]]; then
+        echo "ODF started on "$LOCAL" at $(date)" #>> $LOGFILE
+      else
+        echo "ODF failed to start at $(date)" #>> $LOGFILE
+        # exit 1
+      fi
+  else
+    echo "ODF  running on "$LOCAL" at $(date)" #>> $LOGFILE
+  fi
+}
+
+# Function to start FTS Exporter
+startFTSExporter() {
+  if [[ $(ps -ef | grep -c [O]DFTIExporter)]]; then
+    cd /apps/IBM/ondemand/V9.5/jars
+    nohup java -Djava.library.path=/apps/IBM/ondemand/V9.5/lib64 -jar ODFTIExporter.jar index -configFile odfts.cfg &
+    sleep 1
+    if [[ $? = 0 ]]; then
+      echo "FTS Exporter started on "$LOCAL" at $(date)" #>> $LOGFILE
+    else
+      echo "FTS Exporter failed to start at $(date)" #>> $LOGFILE
+      # exit 1
+    fi
+  else
+    echo "FTS Exporter running on "$LOCAL" at $(date)" #>> $LOGFILE
+  fi
+}
 # END OF FUNCTION LIST ##############################################
 #####################################################################
 
@@ -68,7 +126,9 @@ while (true)
 do
 # Set activeServer to empty
 activeServer=""
-
+# determine activeServer
+arsdocQuery "$LOCAL"
+sleep 20
 checkCMOD "$LOCAL"
 checkCMOD "$REMOTE"
 #for debugging can be removed
@@ -76,7 +136,14 @@ echo "Active is "$activeServer""
 echo "Standby is "$standbyServer""
 
 #Check activeServer to validate all services are running
-
+if [[ "$LOCAL" == "$activeServer" ]]; then
+#startTSM
+startTSM
+#startODF
+startODF
+#startFTSExporter
+startFTSExporter
+fi
 
 # if no active server on either box start CMOD
 if [[ -z "$activeServer" ]]
@@ -87,11 +154,18 @@ if [[ -z "$activeServer" ]]
     touch "$LOCK"
     # Start CMOD on Local server
     "$ARSBIN" arssockd -h "$LOCAL" -S
+    sleep 1
 	if [[ $? == 0 ]]; then
     activeServer="$LOCAL"
     echo "Active Server is $activeServer at $(date)"  #>> $LOGFILE
     echo "Starting other CMOD services"               #>> $LOGFILE
-	#Start TSM, ODF, FTS
+  #Start TSM, ODF, FTS
+  startTSM
+  startODF
+  if [[ $? != 0 ]];then
+    echo "ODF failed to start at $(date)" #>> $LOGFILE
+  fi
+  startFTSExporter
 	#remove lockfile
     rm "$LOCK"
     fi
